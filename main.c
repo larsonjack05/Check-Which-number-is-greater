651c609b2b0a653b0a29d200caeb510bd0d9b76c
Dhruv Patil <dhruvpatil494@gmail.com>
2020-12-02 11:10:22 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+Program to check which number is greater
+Author: Dhruv Patil
+College: Acropolis Indore (IT) Branch
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	int a,b; //Declare 2 Variable of int type
+
+	printf("Enter First Number ");
+	scanf("%d",&a);
+
+  printf("Enter Second Number ");
+	scanf("%d",&b);
+
+  printf("%d",a);
+
+
+
+	return 0;
+}
